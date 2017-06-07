94e1833b6b1f2046e65eb96c00a81cde7f39145f
github username <github email address>
2017-06-07 16:54:18 -0400

    squashed bugs, and added downcase functionality

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,21 +7,12 @@ def get_character_movies_from_api(character)
   character_hash = get_JSON('http://www.swapi.co/api/people/')
 
   character_hash["results"].map do |char|
-    if char["name"] == character
+    if char["name"].downcase == character.downcase
       char["films"].each_with_object([]) do |filmURL, arr|
         arr.push(get_JSON(filmURL))
       end
     end
   end.flatten.compact
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
 def get_JSON(url)
@@ -30,9 +21,12 @@ def get_JSON(url)
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
-  films_hash.each do |film|
+  if films_hash.size == 0
+    puts "You're not a movie star yet!"
+  else
+    films_hash.each do |film|
       puts film["title"]
+    end
   end
 end
 
